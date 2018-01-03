7f3423e433c6e5f1872b68a7246954d7eb1de159
Zhedong Zheng <zzheng@joo-ml1-dev.j.cinglevue.com>
2018-01-03 20:07:41 +0800

    fix size

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ if len(gpu_ids)>0:
 # data.
 #
 data_transforms = transforms.Compose([
-        transforms.Resize(144, interpolation=3),
+        transforms.Resize((288,144), interpolation=3),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ############### Ten Crop        
--- train.py
+++ train.py
@@ -65,7 +65,7 @@ if opt.color_jitter:
 print(transform_train_list)
 
 transform_val_list = [
-        transforms.Resize(size=128,interpolation=3), #Image.BICUBIC
+        transforms.Resize(size=(256,128),interpolation=3), #Image.BICUBIC
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
