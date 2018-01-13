30b7d632737fb419827bf8d4c672c1c0ff5c72b9
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2018-01-13 23:06:18 +1100

    Update train.py

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ parser.add_argument('--data_dir',default='/home/zzheng/Downloads/Market/pytorch'
 parser.add_argument('--train_all', action='store_true', help='use all training data' )
 parser.add_argument('--color_jitter', action='store_true', help='use color jitter in training' )
 parser.add_argument('--batchsize', default=16, type=int, help='batchsize')
-parser.add_argument('--erasing_p', default=0, type=float, help='Random Erasing probability')
+parser.add_argument('--erasing_p', default=0, type=float, help='Random Erasing probability, in [0,1]')
 
 opt = parser.parse_args()
 data_dir = opt.data_dir
@@ -60,10 +60,12 @@ transform_train_list = [
         transforms.RandomCrop((256,128)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        RandomErasing(args.p)
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
 
+if opt.eraing_p>0:
+    transform_train_list = transform_train_list + [RandomErasing(opt.erasing_p)]
+    
 if opt.color_jitter:
     transform_train_list = [transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0)] + transform_train_list
 
