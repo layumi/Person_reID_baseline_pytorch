a1f6677f8705ff107c70ee4cae372e72bdf6ef31
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2018-04-24 16:04:15 +0800

    Update model.py

--- model.py
+++ model.py
@@ -121,6 +121,78 @@ class ft_net_middle(nn.Module):
         x = self.classifier(x)
         return x
 
+# Part Model proposed in Yifan Sun etal. (2018)
+class PCB(nn.Module):
+    def __init__(self, class_num ):
+        super(PCB, self).__init__()
+
+        self.part = 6 # We cut the pool5 to 6 parts
+        model_ft = models.resnet50(pretrained=True)
+        self.model = model_ft
+        self.avgpool = nn.AdaptiveAvgPool2d((self.part,1))
+        self.dropout = nn.Dropout(p=0.5)
+        # remove the final downsample
+        self.model.layer4[0].downsample[0].stride = (1,1)
+        self.model.layer4[0].conv2.stride = (1,1)
+        # define 4 classifier
+        for i in range(self.part):
+            name = 'classifier'+str(i)
+            setattr(self, name, ClassBlock(2048, class_num, True, False, 256))
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+        
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.avgpool(x)
+        x = self.dropout(x)
+        part = {}
+        predict = {}
+        # get four part feature batchsize*2048*4
+        for i in range(self.part):
+            part[i] = torch.squeeze(x[:,:,i])
+            name = 'classifier'+str(i)
+            c = getattr(self,name)
+            predict[i] = c(part[i])
+
+        # sum prediction
+        #y = predict[0]
+        #for i in range(self.part-1):
+        #    y += predict[i+1]
+        y = []
+        for i in range(self.part):
+            y.append(predict[i])
+        return y
+
+class PCB_test(nn.Module):
+    def __init__(self,model):
+        super(PCB_test,self).__init__()
+        self.part = 6
+        self.model = model.model
+        self.avgpool = nn.AdaptiveAvgPool2d((self.part,1))
+        # remove the final downsample
+        self.model.layer4[0].downsample[0].stride = (1,1)
+        self.model.layer4[0].conv2.stride = (1,1)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.avgpool(x)
+        y = x.view(x.size(0),x.size(1),x.size(2))
+        return y
+
 # debug model structure
 #net = ft_net(751)
 net = ft_net_dense(751)
