d212206c0e655c081b1613dc97b33ed35c907658
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2018-07-05 09:23:31 +0800

    Add multi-query evaluation

--- prepare.py
+++ prepare.py
@@ -29,6 +29,24 @@ for root, dirs, files in os.walk(query_path, topdown=True):
         copyfile(src_path, dst_path + '/' + name)
 
 #-----------------------------------------
+#multi-query
+query_path = download_path + '/gt_bbox'
+query_save_path = download_path + '/pytorch/multi-query'
+if not os.path.isdir(query_save_path):
+    os.mkdir(query_save_path)
+
+for root, dirs, files in os.walk(query_path, topdown=True):
+    for name in files:
+        if not name[-3:]=='jpg':
+            continue
+        ID  = name.split('_')
+        src_path = query_path + '/' + name
+        dst_path = query_save_path + '/' + ID[0]
+        if not os.path.isdir(dst_path):
+            os.mkdir(dst_path)
+        copyfile(src_path, dst_path + '/' + name)
+
+#-----------------------------------------
 #gallery
 gallery_path = download_path + '/bounding_box_test'
 gallery_save_path = download_path + '/pytorch/gallery'
