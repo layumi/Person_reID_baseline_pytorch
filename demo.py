c507ae5ec28fb33ec9aa01124cd124331a57ff2e
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2018-08-22 18:19:13 -0700

    Visualize ranking result

---
+++ demo.py
@@ -0,0 +1,90 @@
+import argparse
+import scipy.io
+import torch
+import numpy as np
+import time
+import os
+from torchvision import datasets, models, transforms
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+#######################################################################
+# Evaluate
+parser = argparse.ArgumentParser(description='Demo')
+parser.add_argument('--query_index', default=777, type=int, help='test_image_index')
+parser.add_argument('--test_dir',default='../Market/pytorch',type=str, help='./test_data')
+opts = parser.parse_args()
+
+data_dir = opts.test_dir
+image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ) for x in ['gallery','query']}
+
+#####################################################################
+#Show result
+def imshow(path, title=None):
+    """Imshow for Tensor."""
+    im = plt.imread(path)
+    plt.imshow(im)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+######################################################################
+result = scipy.io.loadmat('pytorch_result.mat')
+query_feature = torch.FloatTensor(result['query_f'])
+query_cam = result['query_cam'][0]
+query_label = result['query_label'][0]
+gallery_feature = torch.FloatTensor(result['gallery_f'])
+gallery_cam = result['gallery_cam'][0]
+gallery_label = result['gallery_label'][0]
+
+multi = os.path.isfile('multi_query.mat')
+
+if multi:
+    m_result = scipy.io.loadmat('multi_query.mat')
+    mquery_feature = torch.FloatTensor(m_result['mquery_f'])
+    mquery_cam = m_result['mquery_cam'][0]
+    mquery_label = m_result['mquery_label'][0]
+    mquery_feature = mquery_feature.cuda()
+
+query_feature = query_feature.cuda()
+gallery_feature = gallery_feature.cuda()
+
+#######################################################################
+# sort the images
+def sort_img(qf,gf):
+    query = qf.view(-1,1)
+    score = torch.mm(gf,query)
+    score = score.squeeze(1).cpu()
+    score = score.numpy()
+    # predict index
+    index = np.argsort(score)  #from small to large
+    index = index[::-1]
+    return index
+
+index = sort_img(query_feature[opts.query_index],gallery_feature)
+
+########################################################################
+# Visualize the rank result
+
+query_path, label = image_datasets['query'].imgs[opts.query_index]
+print(query_path)
+print('Top 10 images are as follow:')
+try: # Visualize Ranking Result 
+    # Graphical User Interface is needed
+    fig = plt.figure()
+    ax = plt.subplot(3,4,1)
+    ax.axis('off')
+    imshow(query_path,'query')
+    for i in range(10):
+        ax = plt.subplot(3,4,i+2)
+        ax.axis('off')
+        img_path, label = image_datasets['gallery'].imgs[index[i]]
+        imshow(img_path,'top-%d'%(i+1))
+        print(img_path)
+except RuntimeError:
+    for i in range(10):
+        img_path = image_datasets.imgs[index[i]]
+        print(img_path[0])
+    print('If you want to see the visualization of the ranking result, graphical user interface is needed.')
+
+fig.savefig("show.png")
