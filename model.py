2a3f1f57671e8d3d685d3623405fe186e2d1a9de
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2018-11-29 12:18:34 -0800

    Add dropout option

--- model.py
+++ model.py
@@ -26,15 +26,20 @@ def weights_init_classifier(m):
 # Defines the new fc layer and classification layer
 # |--Linear--|--bn--|--relu--|--Linear--|
 class ClassBlock(nn.Module):
-    def __init__(self, input_dim, class_num, dropout=True, relu=True, num_bottleneck=512):
+    def __init__(self, input_dim, class_num, droprate, relu=False, bnorm=True, num_bottleneck=512, linear=True, return_f = False):
         super(ClassBlock, self).__init__()
+        self.return_f = return_f
         add_block = []
-        add_block += [nn.Linear(input_dim, num_bottleneck)] 
-        add_block += [nn.BatchNorm1d(num_bottleneck)]
+        if linear:
+            add_block += [nn.Linear(input_dim, num_bottleneck)]
+        else:
+            num_bottleneck = input_dim
+        if bnorm:
+            add_block += [nn.BatchNorm1d(num_bottleneck)]
         if relu:
             add_block += [nn.LeakyReLU(0.1)]
-        if dropout:
-            add_block += [nn.Dropout(p=0.5)]
+        if droprate>0:
+            add_block += [nn.Dropout(p=droprate)]
         add_block = nn.Sequential(*add_block)
         add_block.apply(weights_init_kaiming)
 
@@ -47,19 +52,24 @@ class ClassBlock(nn.Module):
         self.classifier = classifier
     def forward(self, x):
         x = self.add_block(x)
-        x = self.classifier(x)
-        return x
+        if self.return_f:
+            f = x
+            x = self.classifier(x)
+            return x,f
+        else:
+            x = self.classifier(x)
+            return x
 
 # Define the ResNet50-based Model
 class ft_net(nn.Module):
 
-    def __init__(self, class_num ):
+    def __init__(self, class_num, droprate=0.5):
         super(ft_net, self).__init__()
         model_ft = models.resnet50(pretrained=True)
         # avg pooling to global pooling
         model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
         self.model = model_ft
-        self.classifier = ClassBlock(2048, class_num)
+        self.classifier = ClassBlock(2048, class_num, droprate)
 
     def forward(self, x):
         x = self.model.conv1(x)
