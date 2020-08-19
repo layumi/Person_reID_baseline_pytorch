96b73bd5abd65ed7cd0b800249f71a72600146c8
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2020-08-19 21:27:40 +1000

    Update prepare_viper.py

--- prepare_viper.py
+++ prepare_viper.py
@@ -52,20 +52,29 @@ for root, dirs, files in os.walk(train_path, topdown=True):
 
 np.random.seed(0)
 
-for ii in range(5):
+for ii in range(1):
     index = np.random.permutation(632)
-    test_save_path = download_path + '/test%d'%ii
+    index = index[0:316]
+    test_save_path = download_path + '/pytorch/' #'/test%d'%ii
     if not os.path.isdir(test_save_path):
         os.mkdir(test_save_path)
-        os.mkdir(test_save_path + '/query')
-        os.mkdir(test_save_path + '/gallery')
+    os.mkdir(test_save_path + '/train')
+    os.mkdir(test_save_path + '/query')
+    os.mkdir(test_save_path + '/gallery')
 
-    for i in range(316):
+    for i in range(632):
         dir_name = ID_list[i]
-        src_path_1 = train_save_path + '/' + dir_name + '/ca_*.bmp'
-        src_path_2 = train_save_path + '/' + dir_name + '/cb_*.bmp'
-        dst_path_1 = test_save_path+'/query/'
-        dst_path_2 = test_save_path+'/gallery/'
-        os.system('cp %s %s'%(src_path_1, dst_path_1))
-        os.system('cp %s %s'%(src_path_2, dst_path_2))
+        if i in index:
+            src_path_1 = train_save_path + '/' + dir_name
+            dst_path_1 = test_save_path+'/train/'
+            os.system('cp -r  %s %s'%(src_path_1, dst_path_1))
+        else:
+            src_path_1 = train_save_path + '/' + dir_name #+ '/ca_*.bmp'
+            #src_path_2 = train_save_path + '/' + dir_name + '/cb_*.bmp'
+            dst_path_1 = test_save_path+'/query/'
+            #dst_path_2 = test_save_path+'/gallery/' + dir_name
+            os.system('cp -r %s %s'%(src_path_1, dst_path_1))
+
+os.system('cp -r %s  %s'%(test_save_path+'/query/*', test_save_path+'/gallery/'))
+
 
