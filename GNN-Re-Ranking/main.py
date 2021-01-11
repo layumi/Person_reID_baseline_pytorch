b2ff22ac3a873ddafcae5e93f69a396defbe63cf
Xuanmeng Zhang <58422832+Xuanmeng-Zhang@users.noreply.github.com>
2021-01-11 11:33:49 +0800

    Add files via upload

--- GNN-Re-Ranking/main.py
+++ GNN-Re-Ranking/main.py
@@ -1,3 +1,21 @@
+"""
+    Understanding Image Retrieval Re-Ranking: A Graph Neural Network Perspective
+
+    Xuanmeng Zhang, Minyue Jiang, Zhedong Zheng, Xiao Tan, Errui Ding, Yi Yang
+
+    Project Page : https://github.com/Xuanmeng-Zhang/gnn-re-ranking
+
+    Paper: https://arxiv.org/abs/2012.07620v2
+
+    ======================================================================
+   
+    On the Market-1501 dataset, we accelerate the re-ranking processing from 89.2s to 9.4ms
+    with one K40m GPU, facilitating the real-time post-processing. Similarly, we observe 
+    that our method achieves comparable or even better retrieval results on the other four 
+    image retrieval benchmarks, i.e., VeRi-776, Oxford-5k, Paris-6k and University-1652, 
+    with limited time cost.
+"""
+
 import os
 import torch
 import argparse
