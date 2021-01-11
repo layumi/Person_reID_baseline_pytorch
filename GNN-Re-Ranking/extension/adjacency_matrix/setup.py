11970d538b436fa15b1127f563d416fdb1f7c079
Xuanmeng Zhang <58422832+Xuanmeng-Zhang@users.noreply.github.com>
2021-01-11 10:55:18 +0800

    Add files via upload

--- GNN-Re-Ranking/extension/adjacency_matrix/setup.py
+++ GNN-Re-Ranking/extension/adjacency_matrix/setup.py
@@ -1,1 +1,19 @@
+from setuptools import setup, Extension
 
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+
+setup(
+    name='gnn_propagate',
+    ext_modules=[
+        CUDAExtension('gnn_propagate', [
+            'gnn_propagate.cpp',
+            'gnn_propagate_kernel.cu',
+        ]),
+    ],
+    cmdclass={
+        'build_ext':BuildExtension  
+    })
