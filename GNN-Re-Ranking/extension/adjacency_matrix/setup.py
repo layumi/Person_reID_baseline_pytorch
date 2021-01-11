8e90537ef6c85afac0d70434e8d4e2928097adec
Xuanmeng Zhang <58422832+Xuanmeng-Zhang@users.noreply.github.com>
2021-01-11 11:34:30 +0800

    Add files via upload

--- GNN-Re-Ranking/extension/adjacency_matrix/setup.py
+++ GNN-Re-Ranking/extension/adjacency_matrix/setup.py
@@ -1,3 +1,21 @@
+"""
+    Understanding Image Retrieval Re-Ranking: A Graph Neural Network Perspective
+
+    Xuanmeng Zhang, Minyue Jiang, Zhedong Zheng, Xiao Tan, Errui Ding, Yi Yang
+
+    Project Page : https://github.com/Xuanmeng-Zhang/gnn-re-ranking
+
+    Paper: https://arxiv.org/abs/2012.07620v2
+
+    ======================================================================
+   
+    On the Market-1501 dataset, we accelerate the re-ranking processing from 89.2s to 9.4ms
+    with one K40m GPU, facilitating the real-time post-processing. Similarly, we observe 
+    that our method achieves comparable or even better retrieval results on the other four 
+    image retrieval benchmarks, i.e., VeRi-776, Oxford-5k, Paris-6k and University-1652, 
+    with limited time cost.
+"""
+
 from setuptools import setup, Extension
 
 import torch
@@ -7,11 +25,11 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 
 setup(
-    name='gnn_propagate',
+    name='build_adjacency_matrix',
     ext_modules=[
-        CUDAExtension('gnn_propagate', [
-            'gnn_propagate.cpp',
-            'gnn_propagate_kernel.cu',
+        CUDAExtension('build_adjacency_matrix', [
+            'build_adjacency_matrix.cpp',
+            'build_adjacency_matrix_kernel.cu',
         ]),
     ],
     cmdclass={
