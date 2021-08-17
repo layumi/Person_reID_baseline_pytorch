858f880b62f3acc38f0df5d5a81fd67fb22040a7
ronghao233 <88146336+ronghao233@users.noreply.github.com>
2021-08-17 10:27:07 +0800

    Update model.py

--- model.py
+++ model.py
@@ -65,7 +65,7 @@ class ClassBlock(nn.Module):
 # Define the ResNet50-based Model
 class ft_net(nn.Module):
 
-    def __init__(self, class_num, droprate=0.5, stride=2, circle=False):
+    def __init__(self, class_num=751, droprate=0.5, stride=2, circle=False):
         super(ft_net, self).__init__()
         model_ft = models.resnet50(pretrained=True)
         # avg pooling to global pooling
