643577a07051456878f504cfeb1d7d7fc4da0310
mu <91316117+kagawa123@users.noreply.github.com>
2022-01-07 16:14:23 +0800

    Update train.py
    Support linear_num option for DenseNet, HRNet, Swin, EfficientNet and NAS.

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ parser.add_argument('--data_dir',default='../Market/pytorch',type=str, help='tra
 parser.add_argument('--train_all', action='store_true', help='use all training data' )
 parser.add_argument('--color_jitter', action='store_true', help='use color jitter in training' )
 parser.add_argument('--batchsize', default=32, type=int, help='batchsize')
-parser.add_argument('--linear_num', default=512, type=int, help='feature dimension: 512 or 2048 or 0 (linear=False)')
+parser.add_argument('--linear_num', default=512, type=int, help='feature dimension: 512 or default or 0 (linear=False)')
 parser.add_argument('--stride', default=2, type=int, help='stride')
 parser.add_argument('--erasing_p', default=0, type=float, help='Random Erasing probability, in [0,1]')
 parser.add_argument('--use_dense', action='store_true', help='use densenet121' )
@@ -431,15 +431,15 @@ def save_network(network, epoch_label):
 return_feature = opt.arcface or opt.cosface or opt.circle or opt.triplet or opt.contrast or opt.instance or opt.lifted or opt.sphere
 
 if opt.use_dense:
-    model = ft_net_dense(len(class_names), opt.droprate, circle = return_feature)
+    model = ft_net_dense(len(class_names), opt.droprate, circle = return_feature, linear_num=opt.linear_num)
 elif opt.use_NAS:
-    model = ft_net_NAS(len(class_names), opt.droprate)
+    model = ft_net_NAS(len(class_names), opt.droprate, linear_num=opt.linear_num)
 elif opt.use_swin:
-    model = ft_net_swin(len(class_names), opt.droprate, opt.stride, circle = return_feature)
+    model = ft_net_swin(len(class_names), opt.droprate, opt.stride, circle = return_feature, linear_num=opt.linear_num)
 elif opt.use_efficient:
-    model = ft_net_efficient(len(class_names), opt.droprate, circle = return_feature)
+    model = ft_net_efficient(len(class_names), opt.droprate, circle = return_feature, linear_num=opt.linear_num)
 elif opt.use_hr:
-    model = ft_net_hr(len(class_names), opt.droprate, circle = return_feature)
+    model = ft_net_hr(len(class_names), opt.droprate, circle = return_feature, linear_num=opt.linear_num)
 else:
     model = ft_net(len(class_names), opt.droprate, opt.stride, circle = return_feature, ibn=opt.ibn, linear_num=opt.linear_num)
 
